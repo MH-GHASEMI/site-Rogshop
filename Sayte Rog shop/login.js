b7c5e0cc88229a2d18c449b354b6b0cb53ed37a2
MH-GHASEMI <149863431+MH-GHASEMI@users.noreply.github.com>
2023-11-04 12:03:04 +0330

    Add files via upload

---
+++ Sayte Rog shop/login.js
@@ -0,0 +1,13 @@
+var us,pas,phon,cd;
+us = (document.getElementById("inputuser").value);
+phon = Number(document.getElementById("inphone").value);
+pas = (document.getElementById("inputpass").value);
+cd = (document.getElementById("in-cod").value);
+function btnlogin(){
+
+    if (cd=='0741538701')
+    <b>true</b>
+    else
+    <b>try again</b>
+}
+    
